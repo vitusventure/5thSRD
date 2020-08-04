df88a34c859fa499cc7e634540fc66c5328404c0
vitusventure <johnornd@gmail.com>
2020-08-04 15:33:50 -0700

    Make tables responsive in purecss theme

--- plugins/table-styler.py
+++ plugins/table-styler.py
@@ -1,5 +1,6 @@
 import re
 
 def style_tables(output_content, page, config):
-    output_content = re.sub(r'<table>', '<table class="pure-table pure-table-striped">', output_content)
+    output_content = re.sub(r'<table>', '<div class="responsive-table"><table class="pure-table pure-table-striped">', output_content)
+    output_content = re.sub(r'</table>', '</table></div>', output_content)
     return output_content
